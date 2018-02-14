03cb1cac3af51840f4448e7749f9c5b3e235c3fc
Meir Taffel <artgardening@gmail.com>
2018-02-14 16:52:27 +0000

    Allow unsigned user to view bloc_cast views

--- app/controllers/bloc_cast_controller.rb
+++ app/controllers/bloc_cast_controller.rb
@@ -1,4 +1,6 @@
 class BlocCastController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show, :search]
+
   def index
   end
 
--- test/controllers/bloc_cast_controller_test.rb
+++ test/controllers/bloc_cast_controller_test.rb
@@ -2,17 +2,17 @@ require 'test_helper'
 
 class BlocCastControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get bloc_cast_index_url
+    get root_url
     assert_response :success
   end
 
   test "should get show" do
-    get bloc_cast_show_url
+    get show_url
     assert_response :success
   end
 
   test "should get search" do
-    get bloc_cast_search_url
+    get search_url
     assert_response :success
   end
 
