813992e17015c4a5f8d1b6d67c150171e3cc0e2b
Meir Taffel <artgardening@gmail.com>
2018-02-15 09:17:07 +0000

    First basic api call to TMDB

--- app/controllers/bloc_cast_controller.rb
+++ app/controllers/bloc_cast_controller.rb
@@ -2,6 +2,8 @@ class BlocCastController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show, :search]
 
   def index
+    response = HTTParty.get("https://api.themoviedb.org/3/discover/tv?api_key=#{ENV["TMDB_API_KEY"]}&sort_by=popularity.desc")
+    @popular_shows = response['results']
   end
 
   def show
