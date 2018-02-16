96af7d53df1e97686c71a4df6b32b9fe47013969
Meir Taffel <artgardening@gmail.com>
2018-02-16 12:01:41 +0000

    Add show view

--- app/controllers/bloc_cast_controller.rb
+++ app/controllers/bloc_cast_controller.rb
@@ -7,6 +7,9 @@ class BlocCastController < ApplicationController
   end
 
   def show
+    response = HTTParty.get("https://api.themoviedb.org/3/tv/#{params[:tv_id]}?api_key=#{ENV["TMDB_API_KEY"]}")
+    puts response
+    @tv_show = response
   end
 
   def search
--- app/helpers/bloc_cast_helper.rb
+++ app/helpers/bloc_cast_helper.rb
@@ -1,2 +1,5 @@
 module BlocCastHelper
+    def in_production?(actively_producing)
+        actively_producing ? "In Production" : "Not in Production" 
+    end
 end
